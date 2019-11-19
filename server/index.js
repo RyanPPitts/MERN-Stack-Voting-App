22430ba0f05c87dfad964819cc44fc3d7b7557cf
RyanPPitts <ryanpitts26@gmail.com>
2019-11-18 16:10:55 -0800

    Adding handlers for errors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ console.log ('hello world lets get this going');
 
 // creating our server connection with express
 const express = require('express');
+const handle = require('./handlers/index');
 
 const app = express();
 const port = 4000;
@@ -10,19 +11,9 @@ app.get('/', (req,res) => res.send('hello world'));
 
 
 // create error 404 page if requested page by user is not available
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-
-    next(err);
-
-});
+app.use(handle.notFound);
 
 // final end point if the 404 error is not displayed
-app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({ 
-    err: err.message || "Something is wrong"
-    });
-});
+app.use(handle.errors);
 
 app.listen(port, console.log(`Server started on port ${port}`));

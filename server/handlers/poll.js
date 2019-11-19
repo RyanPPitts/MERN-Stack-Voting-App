626879b0d4b6d6fc9fdd7243eaa0074349cf1a23
RyanPPitts <ryanpitts26@gmail.com>
2019-11-18 20:33:09 -0800

    adding routes and handles to the server

--- server/handlers/poll.js
+++ server/handlers/poll.js
@@ -14,6 +14,17 @@ exports.showPolls = async (req, res, next) => {
   }
 };
 
+exports.userPolls = async (req, res, next) => {
+  try {
+    const { id } = req.decoded;
+    const user = await db.User.findById(id).populate("polls");
+    res.status(200).json(user.polls);
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
+};
+
 exports.createPoll = async (req, res, next) => {
   try {
     console.log(req.decoded);
@@ -28,6 +39,38 @@ exports.createPoll = async (req, res, next) => {
     user.polls.push(poll._id);
     await user.save();
     res.status(201).json({ ...poll._doc, user: user._id });
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
+};
+
+exports.getPoll = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const poll = await db.Poll.findById(id).populate("user", [
+      "username",
+      "id"
+    ]);
+    if (!poll) throw new Error("No poll found");
+    res.status(200).json(poll);
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
+};
+
+exports.deletePoll = async (req, res, next) => {
+  try {
+    const { id: pollId } = req.params;
+    const { id: userId } = req.decoded;
+    const poll = await db.Poll.findById(pollId);
+    if (!poll) throw new Error("No poll found");
+    if (poll.user.toString() !== userId) {
+      throw new Error("Unauthorized access");
+    }
+    await poll.remove();
+    res.status(202).json(poll);
   } catch (err) {
     err.status = 400;
     next(err);
--- server/routes/poll.js
+++ server/routes/poll.js
@@ -6,6 +6,14 @@ const auth = require("../middlewares/auth");
 router
   .route("/")
   .get(handle.showPolls) // show all polls
-  .post(handle.createPoll);
+  .post(auth, handle.createPoll);
+
+router.get("/user", auth, handle.usersPolls);
+
+router
+  .route("/:id")
+  .get(handle.getPoll)
+  .post(auth, handle.vote)
+  .delete(auth, handle.deletePoll);
 
 module.exports = router;
